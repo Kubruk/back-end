3d8ce47a4d1c82a500010c114c6311d6863f95a2
Kubruk <99119267+Kubruk@users.noreply.github.com>
2022-06-19 20:17:10 +0200

    auth routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 const app = express()
 
 app.use(express.static("public"))
+app.use("/api/auth", require("./routes/auth"))
 
 app.listen(process.env.PORT, () => {
     console.log(`Running on port: ${process.env.PORT}`)
