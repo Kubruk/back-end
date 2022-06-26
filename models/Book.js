4a5b30c65284d54ca6d0c600219808a1b97b2d70
Kubruk <99119267+Kubruk@users.noreply.github.com>
2022-06-26 12:03:42 +0200

    added timestamps and serializer

--- models/Book.js
+++ models/Book.js
@@ -3,28 +3,39 @@ const { MODEL_NAME: userModelName } = require("./User");
 
 const MODEL_NAME = "Book";
 
-const BookSchema = Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: userModelName,
-    required: true,
+const BookSchema = Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: userModelName,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    portrait: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    genres: {
+      type: Array,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  portrait: {
-    type: String,
-    required: true,
-    default: "",
-  },
-  genres: {
-    type: Array,
-  },
+  {
+    timestamps: true,
+  }
+);
+
+BookSchema.method("toJSON", function () {
+  const { _id, __v, ...object } = this.toObject();
+  object.id = _id;
+  return object;
 });
 
 const bookSchema = model(MODEL_NAME, BookSchema);
--- models/User.js
+++ models/User.js
@@ -1,21 +1,26 @@
 const { Schema, model } = require("mongoose");
 
 const MODEL_NAME = "User";
-const UserSchema = Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const userSchema = model(MODEL_NAME, UserSchema);
 
